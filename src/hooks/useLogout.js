39e9fbabd4eeabc231515c738717f6aa2ad0e16f
Sang-Truong20 <sangtpse171049@fpt.edu.vn>
2025-07-08 19:04:31 +0700

    add skeleton + clear cache for packages

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,15 +1,18 @@
+import { useQueryClient } from '@tanstack/react-query';
 import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 import { PATH_NAME } from '../constants';
 
 const useLogout = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const logout = () => {
     localStorage.removeItem('isAuthenticated');
     localStorage.removeItem('userId');
     Cookies.remove('accessToken');
     Cookies.remove('refreshToken');
+    queryClient.clear();
     navigate(PATH_NAME.AUTH);
   };
 
